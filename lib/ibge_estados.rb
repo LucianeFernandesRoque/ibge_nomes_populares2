1f5878263907f177515bd4d9603a539abf3ac525
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2021-04-23 21:44:25 -0300

    wip bd

--- lib/ibge_estados.rb
+++ lib/ibge_estados.rb
@@ -9,11 +9,23 @@ class IbgeEstados
   end
 
   def self.estados_all
+    db = SQLite3::Database.open "db/database.db"
     response = Faraday.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
     json = JSON.parse(response.body, symbolize_names: true)
     json.map do |dados|
+    db.results_as_hash = true
+      db.execute "SELECT id, sigla, nome FROM estados"
+      db.close
+      estados.map {|estados| new(id: estados['id'], sigla: estados['sigla'], nome: estados['nome']) }
       @dados = dados[:id], dados[:sigla], dados[:nome]
     end
+  end
+
+  def save_to_db
+    db = SQLite3::Database.open "db/database.db"
+    db.execute "INSERT INTO estados VALUES('#{ id }', '#{ sigla }','#{ nome }')"
+    db.close
+    self
   end
 
   def self.table_estado
