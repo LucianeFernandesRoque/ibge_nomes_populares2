db367df426628ce6bf55ce8a7873b2bdd378086b
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2021-04-29 00:19:03 -0300

    wip test double

--- spec/requests/ibge_estados_spec.rb
+++ spec/requests/ibge_estados_spec.rb
@@ -5,12 +5,17 @@ require 'json'
 
 RSpec.describe 'IbgeEstados' do
   it 'estados_all methods' do
+    
     @response_body = [{ "id": 12, "sigla": 'AC', "nome": 'Acre', "regiao": { "id": 1, "sigla": 'N', "nome": 'Norte' }}]
-    allow(Faraday).to receive(:post).and_receive(@response_body)
+    resp = class_double('response_body', status: 200)
+    Faraday.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+
+
+    allow(@response_body).to receive(:get).with(Faraday).and_return(@response_body)
     id = 12
     sigla = 'AC'
     nome = 'Acre'
     @dados = IbgeEstados.estados_all
-    expect(@dados).to eq(id, sigla, nome)
+    expect(@dados).to eq([id, sigla, nome])
   end
 end
