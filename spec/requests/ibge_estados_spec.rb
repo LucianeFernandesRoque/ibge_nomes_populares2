bab3b74abe4537b7ec4b70e8f393fcecc16e5c12
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2021-04-29 22:01:20 -0300

    mock IbgeEstados ok

--- spec/requests/ibge_estados_spec.rb
+++ spec/requests/ibge_estados_spec.rb
@@ -4,18 +4,18 @@ require 'faraday'
 require 'json'
 
 RSpec.describe 'IbgeEstados' do
-  it 'estados_all methods' do
-    
-    @response_body = [{ "id": 12, "sigla": 'AC', "nome": 'Acre', "regiao": { "id": 1, "sigla": 'N', "nome": 'Norte' }}]
-    resp = class_double('response_body', status: 200)
-    Faraday.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
-
-
-    allow(@response_body).to receive(:get).with(Faraday).and_return(@response_body)
-    id = 12
-    sigla = 'AC'
-    nome = 'Acre'
-    @dados = IbgeEstados.estados_all
-    expect(@dados).to eq([id, sigla, nome])
+  context 'api validates' do
+    it 'estados_all methods' do
+      @response_body = [{ "id": 12, "sigla": 'AC', "nome": 'Acre',
+                          "regiao": { "id": 1, "sigla": 'N', "nome": 'Norte' } }]
+      response = class_double('response', body: @response_body, status: 200)
+      url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome'
+      allow(Faraday).to receive(:get).and_return(response)
+      id = 12
+      sigla = 'AC'
+      nome = 'Acre'
+      @dados = IbgeEstados.estados_all
+      expect(@dados).to eq([[12, 'AC', 'Acre']])
+    end
   end
 end
